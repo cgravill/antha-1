5e9bea9ffa3f6cc0e764a8c90eb6cc1d84f285f4
Matthew Gregg <m.gregg@synthace.com>
2019-03-06 17:41:19 +0000

    Reorganizing support files.

--- workflow/v1point2/migrate.go
+++ workflow/v1point2/migrate.go
@@ -4,21 +4,61 @@ import (
 	"encoding/json"
 	"errors"
 	"fmt"
-	"io"
+	"os"
+	"path/filepath"
+
+	"github.com/antha-lang/antha/logger"
+	"github.com/antha-lang/antha/workflow"
 )
 
-// WorkflowFromReaders gets a workflow from supplied readers.
-func MigrateFromReaders(rs ...io.ReadCloser) (*Workflow, error) {
-	for _, r := range rs {
+// MigrateWorkflow moves an existing
+func MigrateWorkflow(logger *logger.Logger, supportWorkflows []string, migrate string, migrateTo string) (string, error) {
+	migrateTo = correctMigrateTo(migrate, migrateTo)
+	logger.Log(fmt.Sprintf("Migrating %s, output to %s\n", migrate, migrateTo))
+
+	if baseWf, err := baseWorkflow(supportWorkflows); err != nil {
+		return "", err
+	} else if _, err := readWorkflow(migrate); err != nil {
+		return "", err
+	} else {
+		baseWf.WriteToFile(migrateTo)
+		return "", errors.New("failed to migrate file")
+	}
+}
+
+func baseWorkflow(paths []string) (*workflow.Workflow, error) {
+	if len(paths) == 0 {
+		return &workflow.Workflow{}, nil
+	}
+
+	if rs, err := workflow.ReadersFromPaths(paths); err != nil {
+		return nil, err
+	} else if wf, err := workflow.WorkflowFromReaders(rs...); err != nil {
+		return nil, err
+	} else {
+		return wf, nil
+	}
+}
+
+func correctMigrateTo(migrate string, migrateTo string) string {
+	if migrateTo == "" {
+		return filepath.Join(filepath.Dir(migrate), "migrated_"+filepath.Base(migrate))
+	} else {
+		return migrateTo
+	}
+}
+
+func readWorkflow(path string) (*Workflow, error) {
+	if r, err := os.Open(path); err != nil {
+		return nil, err
+	} else {
 		defer r.Close()
 		wf := &Workflow{}
 		dec := json.NewDecoder(r)
 		if err := dec.Decode(wf); err != nil {
 			return nil, err
+		} else {
+			return wf, nil
 		}
-		fmt.Printf("Read workflow %s \n", wf.Properties.Description)
-		fmt.Printf("Read workflow version %s \n", wf.Version)
-		return wf, nil
 	}
-	return nil, errors.New("no files were supplied")
 }
