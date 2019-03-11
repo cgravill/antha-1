db56e2cd670e403d39c35f8be691fc4c3544c1c0
Matthew Gregg <m.gregg@synthace.com>
2019-03-11 16:43:10 +0000

    Move to a migrater object

---
+++ workflow/v1point2/migrater.go
@@ -0,0 +1,87 @@
+package v1point2
+
+import (
+	"encoding/json"
+	"io"
+	"os"
+
+	"github.com/antha-lang/antha/logger"
+	"github.com/antha-lang/antha/workflow"
+)
+
+type Migrater struct {
+	Logger       *logger.Logger
+	BaseWorkflow *workflow.Workflow
+	OldWorkflow  *Workflow
+}
+
+// NewMigrater creates a new migration object
+func NewMigrater(logger *logger.Logger, merges []string, migrate io.ReadCloser) (*Migrater, error) {
+
+	if wf, err := baseWorkflow(merges); err != nil {
+		return nil, err
+	} else if owf, err := readWorkflow(migrate); err != nil {
+		return nil, err
+	} else {
+		return &Migrater{
+			Logger:       logger,
+			BaseWorkflow: wf,
+			OldWorkflow:  owf,
+		}, nil
+	}
+}
+
+// MigrateAll perform all migration steps
+func (m *Migrater) MigrateAll() error {
+	if err := m.migrateParameters(); err != nil {
+		return err
+	}
+
+	return nil
+}
+
+func (m *Migrater) migrateParameters() error {
+	m.BaseWorkflow.JobId = workflow.JobId(m.OldWorkflow.Properties.Name)
+
+	if m.BaseWorkflow.Meta.Rest == nil {
+		m.BaseWorkflow.Meta.Rest = make(map[string]interface{})
+	}
+
+	m.BaseWorkflow.Meta.Rest["Description"] = m.OldWorkflow.Properties.Description
+
+	return nil
+}
+
+func (m *Migrater) WriteTo(target string) error {
+	if target == "" {
+		_, err := m.BaseWorkflow.WriteTo(os.Stdout)
+		return err
+	} else {
+		return m.BaseWorkflow.WriteToFile(target)
+	}
+}
+
+func baseWorkflow(paths []string) (*workflow.Workflow, error) {
+	if len(paths) == 0 {
+		return &workflow.Workflow{}, nil
+	}
+
+	if rs, err := workflow.ReadersFromPaths(paths); err != nil {
+		return nil, err
+	} else if wf, err := workflow.PartialWorkflowFromReaders(rs...); err != nil {
+		return nil, err
+	} else {
+		return wf, nil
+	}
+}
+
+func readWorkflow(r io.ReadCloser) (*Workflow, error) {
+	defer r.Close()
+	wf := &Workflow{}
+	dec := json.NewDecoder(r)
+	if err := dec.Decode(wf); err != nil {
+		return nil, err
+	} else {
+		return wf, nil
+	}
+}
