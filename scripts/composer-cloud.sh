6f2ae4107bff227247968cb75ffa84e0e895894f
Matthew Sackman <m.sackman@synthace.com>
2019-05-29 17:02:55 +0100

    Tweak composer-cloud script because I fear this may be the cause of the weird failures in the cloud

--- scripts/composer-cloud.sh
+++ scripts/composer-cloud.sh
@@ -11,11 +11,14 @@ DATA_DIR=${DATA_DIR:-/data}
 
 # (See https://github.com/Synthace/microservice/tree/master/cmd/ambassador )
 { while ! [[ -r $DATA_DIR/inputReady ]]; do sleep 0.1; done; }
-< $DATA_DIR/inputReady
-trap "{ echo > $DATA_DIR/outputReady; }" EXIT
+dd if=$DATA_DIR/inputReady of=/dev/null bs=1 count=1
+trap "{ dd if=/dev/zero of=$DATA_DIR/outputReady bs=1 count=1; }" EXIT
 
+find $DATA_DIR -type f
 $MIGRATE -from=${DATA_DIR}/input/workflow/request.pb -outdir=${DATA_DIR}/scratch -format=protobuf - <<<$WF_JSON
 
 cp -a ${DATA_DIR}/scratch/* ${DATA_DIR}/input
 
+find $DATA_DIR -type f
 $COMPOSER -indir=${DATA_DIR}/input -outdir=${DATA_DIR}/output
+find $DATA_DIR -type f
